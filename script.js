95cd0d3a7b44be1f51ec1c7b7f635c920161706d
Hema Sree <hemav513922@gmail.com>
2024-05-13 18:31:49 +0530

    Added dark/light mode

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const nav = document.querySelector(".nav"),
   searchIcon = document.querySelector("#searchIcon"),
   navOpenBtn = document.querySelector(".navOpenBtn"),
   navCloseBtn = document.querySelector(".navCloseBtn");
+const toggleButton = document.getElementById("theme-toggle");
+const body = document.body;
 
 searchIcon.addEventListener("click", () => {
   nav.classList.toggle("openSearch");
@@ -20,31 +22,43 @@ navOpenBtn.addEventListener("click", () => {
 navCloseBtn.addEventListener("click", () => {
   nav.classList.remove("openNav");
 });
+// Toggle between "toggle off and toggle on"//.
+toggleButton.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  body.classList.toggle("light-mode");
 
+  if (body.classList.contains("light-mode")) {
+    toggleButton.classList.remove("bi-toggle-off");
+    toggleButton.classList.add("bi-toggle-on");
+  } else {
+    toggleButton.classList.remove("bi-toggle-on");
+    toggleButton.classList.add("bi-toggle-off");
+  }
+});
 
-document.addEventListener("DOMContentLoaded", function() {
-  const discordButton = document.getElementById('discord-button');
-  const discordPopup = document.getElementById('discord-popup');
-  const closeBtn = document.getElementById('close-btn');
+document.addEventListener("DOMContentLoaded", function () {
+  const discordButton = document.getElementById("discord-button");
+  const discordPopup = document.getElementById("discord-popup");
+  const closeBtn = document.getElementById("close-btn");
 
-  discordButton.addEventListener('click', function() {
-    discordPopup.style.display = 'block';
+  discordButton.addEventListener("click", function () {
+    discordPopup.style.display = "block";
   });
 
-  closeBtn.addEventListener('click', function() {
-    discordPopup.style.display = 'none';
+  closeBtn.addEventListener("click", function () {
+    discordPopup.style.display = "none";
   });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-  const discordPopup = document.getElementById('discord-popup');
-  const closeBtn = document.getElementById('close-btn');
+document.addEventListener("DOMContentLoaded", function () {
+  const discordPopup = document.getElementById("discord-popup");
+  const closeBtn = document.getElementById("close-btn");
 
-  discordButton.addEventListener('click', function() {
-    discordPopup.style.display = 'block';
+  discordButton.addEventListener("click", function () {
+    discordPopup.style.display = "block";
   });
 
-  closeBtn.addEventListener('click', function() {
-    discordPopup.style.display = 'none';
+  closeBtn.addEventListener("click", function () {
+    discordPopup.style.display = "none";
   });
 });
