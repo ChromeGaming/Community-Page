f6c3ae756dea0e2f0aad4005cd263a998380ae3c
Saipranathi <reshamsai150@gmail.com>
2024-05-18 19:05:52 +0530

    done

--- script.js
+++ script.js
@@ -186,3 +186,23 @@ subscriptionForm.addEventListener("submit", function (event) {
     success();
   }
 });
+// start
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// scroll to top button
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
