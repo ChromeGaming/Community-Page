dfe4f4fa6c71fa7217ab526fec285f124562cbe3
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-05-29 20:58:52 +0530

    Update package.json with ejs dependency and fix file paths for CSS and converted .html to .ejs

--- server/index.js
+++ server/index.js
@@ -1,12 +1,36 @@
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
-	res.send("Hello World");
+	res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+	res.render("about.ejs");
+});
+
+app.get("/contribute", (req, res) => {
+	res.render("contribute.ejs");
+});
+
+app.get("/play", (req, res) => {
+	res.render("play.ejs");
+});
+
+app.get("/playgames", (req, res) => {
+	res.render("playgames.ejs");
+});
+
+app.get("/products", (req, res) => {
+	res.render("products.ejs");
 });
 
 app.listen(3000, () => {
